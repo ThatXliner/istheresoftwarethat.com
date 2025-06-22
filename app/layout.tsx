5a6a455a3864c0cd70c633011a894bd9e3783341
ThatXliner <bryan.hu.2020@gmail.com>
2025-06-22 08:20:37 -0700

    Install TanStack Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ import Footer from "./Footer";
 // import DataProviders from "./DataProviders";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,7 +22,7 @@ export const metadata: Metadata = {
   title: "Is There Software That...",
   description: "Find great software",
 };
-
+const queryClient = new QueryClient();
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -36,11 +37,12 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="flex flex-col min-h-screen bg-slate-50">
-          {/* <DataProviders> */}
-          <Header isLoggedIn={!(error || !data?.user)} />
-          <main className="flex-grow">{children}</main>
-          {/* </DataProviders> */}
-          <Footer />
+          <QueryClientProvider client={queryClient}>
+            <Header isLoggedIn={!(error || !data?.user)} />
+            <main className="flex-grow">{children}</main>
+            <Footer />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </div>
       </body>
     </html>
