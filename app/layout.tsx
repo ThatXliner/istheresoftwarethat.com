6131bec93f2fcd8e9d7b7a6a17848c1382768bbd
ThatXliner <bryan.hu.2020@gmail.com>
2025-06-22 08:43:50 -0700

    Moved QueryProvider to a client component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,7 @@ import Footer from "./Footer";
 // import DataProviders from "./DataProviders";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Providers from "./providers";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,7 +21,7 @@ export const metadata: Metadata = {
   title: "Is There Software That...",
   description: "Find great software",
 };
-const queryClient = new QueryClient();
+
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -37,12 +36,11 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="flex flex-col min-h-screen bg-slate-50">
-          <QueryClientProvider client={queryClient}>
+          <Providers>
             <Header isLoggedIn={!(error || !data?.user)} />
             <main className="flex-grow">{children}</main>
             <Footer />
-            <ReactQueryDevtools initialIsOpen={false} />
-          </QueryClientProvider>
+          </Providers>
         </div>
       </body>
     </html>
