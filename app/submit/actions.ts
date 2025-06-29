4a048ade0ab775b1a6335cd9c03f2d1b6d54f538
ThatXliner <bryan.hu.2020@gmail.com>
2025-06-29 18:31:40 -0500

    wip and not implementing for now

--- app/submit/actions.ts
+++ app/submit/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { redirect } from "next/navigation";
 
+// SECURITY: make sure the service role key isn't exactly f
 export async function submit(formData: FormData): Promise<void> {
   console.log(formData);
   return redirect("/submit/success");
